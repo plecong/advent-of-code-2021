7293242e0cc83aa61c024b44f955dfc80937123b
Phuong LeCong <phuong@lecong.org>
2021-12-03 07:38:29 -0800

    Day 1 Part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
-fn day01_input(contents: &str) -> Vec<i32> {
+fn day01_input() -> Vec<i32> {
+    let contents = fs::read_to_string("./day01/input/input1.txt").unwrap();
     contents
         .lines()
         .filter_map(|line| line.trim().parse::<i32>().ok())
@@ -8,14 +9,32 @@ fn day01_input(contents: &str) -> Vec<i32> {
 }
 
 fn main() {
-    let contents = fs::read_to_string("./day01/input/input1.txt").expect("Error reading input");
-    let input = day01_input(&contents);
+    let input = day01_input();
 
-    let count = input
+    let part1_result = part1(&input);
+    println!("Part 1: {}", part1_result);
+
+    let part2_result = part2(&input);
+    println!("Part 2: {}", part2_result);
+}
+
+fn increase_count(measures: &Vec<i32>) -> usize {
+    measures
         .windows(2)
         .map(|pair| pair[1] - pair[0] > 0)
         .filter(|&x| x)
-        .count();
+        .count()
+}
 
-    println!("{}", count)
+fn part1(input: &Vec<i32>) -> usize {
+    increase_count(&input)
+}
+
+fn part2(input: &Vec<i32>) -> usize {
+    let measures: Vec<i32> = input
+        .windows(3)
+        .map(|triplet| triplet[0] + triplet[1] + triplet[2])
+        .collect();
+
+    increase_count(&measures)
 }
